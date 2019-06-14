6351ffb4f43b0ff3578227d5c1cbd358dd02cd96
siangyeh8818 <siangyeh8818@gmail.com>
2019-06-14 13:24:31 +0800

    fix: path error on build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #mac 版的binary
-go build  -o getImageLatestTag-mac source/*.go
+go build  -o getImageLatestTag-mac sourcecode/*.go
 
 #Linux版的binary
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -o getImageLatestTag-linux source/*.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -o getImageLatestTag-linux sourcecode/*.go
 
